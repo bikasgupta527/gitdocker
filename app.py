3940da121a85fccbe0603dc15d598a75ff7c906a
Bikas Gupta <52459462+bikasgupta527@users.noreply.github.com>
2021-06-26 17:37:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask
+from flask_restful import Resource, Api, reqparse
+
 app = Flask(__name__)
+value = {}
+api = Api(app)
+parser = reqparse.RequestParser()
 
-@app.route('/')
-def hello_world():
-    return 'Flask Dockerized'
+class FlaskAppTest(Resource):
+  def get(self):
+    return "This response is coming from get"
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+  def post(self):
+    parser.add_argument("text")
+    args = parser.parse_args()
+    val = args["text"]
+    value = "Hello your text was "+str(val) 
+    return value,201
+
+api.add_resource(FlaskAppTest, '/')
